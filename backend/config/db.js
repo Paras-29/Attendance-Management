23a7dd5421b5d1c4506a11de724a3674d901900c
Paras-29 <gulvanshiparas123@gmail.com>
2025-05-21 13:04:48 +0530

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGODb_URI, {
-     useNewUrlParser: true,
-  useUnifiedTopology: true,
+   
     });
     console.log("Connected to MongoDB");
   } catch (error) {
