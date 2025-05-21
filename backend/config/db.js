9aa73407652e4df506631d57e1e52496e66be0c5
Paras-29 <gulvanshiparas123@gmail.com>
2025-05-21 13:15:56 +0530

    mongouri

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGODb_URI, {
+    await mongoose.connect(process.env.MONGODB_URI, {
    
     });
     console.log("Connected to MongoDB");
